2fe696b1ead5e4532487fce7518f9f05020b20fd
mattakvshi <ytube9504@gmail.com>
2024-03-31 21:15:56 +0300

    add EmergencyType

---
+++ src/helpers/EmergencyType.js
@@ -0,0 +1,33 @@
+const emergencyType = [
+	{
+		id: '1',
+		title: 'Earthquake',
+		color: '#2D384A',
+	},
+	{
+		id: '2',
+		title: 'Flood',
+		color: '#236',
+	},
+	{
+		id: '3',
+		title: 'Tsunami',
+		color: '#214678',
+	},
+	{
+		id: '4',
+		title: 'Hurricane',
+		color: '#5479',
+	},
+	{
+		id: '5',
+		title: 'Forest fire',
+		color: '#FF4C2B',
+	},
+	{
+		id: '6',
+		title: 'terrorist attack',
+		color: '#9654',
+	},
+];
+export { emergencyType };
--- src/helpers/NotificationTemplatesList.js
+++ src/helpers/NotificationTemplatesList.js
@@ -3,27 +3,34 @@ const templatesList = [
 		id: '1',
 		title: 'Earthquake of small power',
 		selectedImg: `${process.env.PUBLIC_URL}/tmpTemplatesImg/image6.png`,
+		emergencyType: '1',
 	},
 	{
 		id: '2',
 		title: 'Djmfe kjqlf qqwlj',
+		emergencyType: '3',
 	},
 	{
 		id: '3',
 		title: 'The forest is burning near ...',
 		selectedImg: `${process.env.PUBLIC_URL}/tmpTemplatesImg/image7.png`,
+		emergencyType: '5',
 	},
 	{
 		id: '4',
 		title: 'Ejlkj ekwjll',
+		emergencyType: '2',
 	},
 	{
 		id: '5',
 		title: 'Flkjfn jlq;ej',
+		emergencyType: '4',
 	},
 	{
 		id: '6',
 		title: 'Efklj wlkjrfel',
+		emergencyType: '6',
 	},
 ];
+
 export { templatesList };
