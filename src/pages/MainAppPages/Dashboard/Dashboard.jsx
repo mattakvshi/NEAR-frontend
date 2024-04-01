230722bb8b387d8de4b8d88729a3b28aa85e3800
mattakvshi <ytube9504@gmail.com>
2024-04-01 19:47:40 +0300

    continue to make up the Dashboard

--- src/pages/MainAppPages/Dashboard/Dashboard.jsx
+++ src/pages/MainAppPages/Dashboard/Dashboard.jsx
@@ -1,8 +1,12 @@
 import './dashboard.css';
 
 import TemplateListItem from '../../../components/MainAppComponents/DashboardComponents/TemplateListItem/TemplateListItem';
-
 import { templatesList } from '../../../helpers/NotificationTemplatesList';
+
+import bell from '../../../img/mdi_bell.png';
+import groupsImg from '../../../img/NEAR-user-group.png';
+import submitedTemplatesImg from '../../../img/NEAR-sentNotification-icon копия.png';
+import sos from '../../../img/MainAppIcon/Group 33514.png';
 
 const Dashboard = ({ currentUser }) => {
 	const currentTemplatesList = () => {
@@ -21,17 +25,49 @@ const Dashboard = ({ currentUser }) => {
 		<div className='dashboard-wrapper'>
 			<div className='actions-row'>
 				<div className='actions-row__item'>
-					<div className='item-block-type1'></div>
+					<div className='item-block-type1'>
+						<img className='tools-img-top-right bell' src={bell} alt='bell' />
+						<h5 className='tools-text text-type1'>
+							Create a new
+							<br /> notification
+							<br /> template
+						</h5>
+					</div>
 				</div>
 				<div className='actions-row__item'>
-					<div className='item-block-type1'></div>
+					<div className='item-block-type1'>
+						<img
+							className='tools-img-top-right groupsImg'
+							src={groupsImg}
+							alt='groupsImg'
+						/>
+						<h5 className='tools-text text-type1'>
+							Create new
+							<br /> group
+						</h5>
+					</div>
 				</div>
 				<div className='actions-row__item'>
-					<div className='item-block-type2'></div>
+					<div className='item-block-type2'>
+						<h5 className='tools-text text-type2'>
+							Submitted
+							<br /> Templates
+						</h5>
+						<img
+							className='tools-center-img1'
+							src={submitedTemplatesImg}
+							alt='submitedTemplatesImg'
+						/>
+					</div>
 				</div>
 				<div className='actions-row__item'>
-					<div className='item-block-type3'>
-						<div className='item-block-type4'></div>
+					<div className='item-block-type1'>
+						<div className='item-block-type3'>
+							<img className='tools-center-img2' src={sos} alt='sos' />
+							<p className='text-type3'>
+								Click on me if you need emergency help!
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
