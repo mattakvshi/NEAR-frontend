7f8da3528fc05b5a928b8d60f125c5bd9795b279
mattakvshi <ytube9504@gmail.com>
2024-04-02 21:32:03 +0300

    updat4e NotificationTemplateList.js

--- src/helpers/NotificationTemplatesList.js
+++ src/helpers/NotificationTemplatesList.js
@@ -4,32 +4,38 @@ const templatesList = [
 		title: 'Earthquake of small power',
 		selectedImg: `${process.env.PUBLIC_URL}/tmpTemplatesImg/image6.png`,
 		emergencyType: '1',
+		date: 'Mar 9, 2023',
 	},
 	{
 		id: '2',
 		title: 'Djmfe kjqlf qqwlj',
 		emergencyType: '3',
+		date: 'Apr 1, 2024',
 	},
 	{
 		id: '3',
 		title: 'The forest is burning near ...',
 		selectedImg: `${process.env.PUBLIC_URL}/tmpTemplatesImg/image7.png`,
 		emergencyType: '5',
+		date: 'Mar 2, 2023',
 	},
 	{
 		id: '4',
 		title: 'Ejlkj ekwjll',
 		emergencyType: '2',
+		date: 'Apr 2, 2024',
 	},
 	{
 		id: '5',
 		title: 'Flkjfn jlq;ej',
 		emergencyType: '4',
+		date: 'Feb 29, 2024',
 	},
 	{
 		id: '6',
 		title: 'Efklj wlkjrfel',
 		emergencyType: '6',
+		date: 'Jan 11, 2024',
 	},
 ];
 
