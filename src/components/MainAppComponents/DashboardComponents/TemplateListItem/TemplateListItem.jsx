707719c0378240cb188ff16971bc1d053f1e5f02
mattakvshi <ytube9504@gmail.com>
2024-04-02 21:31:12 +0300

    udate TemplateListItem.jsx

--- src/components/MainAppComponents/DashboardComponents/TemplateListItem/TemplateListItem.jsx
+++ src/components/MainAppComponents/DashboardComponents/TemplateListItem/TemplateListItem.jsx
@@ -1,8 +1,21 @@
-const TemplateListItem = ({ title }) => {
+import './templateListItem.css';
+
+import ellipsis from '../../../../img/MainAppIcon/VectorDotDotDot.svg';
+
+const TemplateListItem = ({ title, selectedImg, emergencyType, date }) => {
 	return (
-		<li>
-			<div>{title}</div>
-			<div></div>
+		<li className='template-wrapper'>
+			<div className='template-wrapper__column'>
+				<img className='template-img' src={selectedImg} alt='templateImg' />
+			</div>
+			<div className='template-wrapper__column'>
+				<h5>{title}</h5>
+				<p>{date}</p>
+			</div>
+			<div className='template-wrapper__column'></div>
+			<div className='template-wrapper__column'>
+				<img src={ellipsis} alt='ellipsis' />
+			</div>
 		</li>
 	);
 };
